9d948a51ee2edd0da7c740fdc81fc63251a8430c
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-06-24 22:22:23 -0300

    att

---
+++ src/hooks/useRoom.ts
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
+
+type FirebaseQuestions = Record<string, {
+    author: {
+        name: string;
+        avatar: string;
+    };
+    content: string;
+    isAnswered:boolean;
+    isHighlighted: boolean;
+    likes: Record<string, {
+        authorID: string;
+    }>
+}>
+
+type QuestionType = {
+    id: string;
+    author: {
+        name: string;
+        avatar: string;
+    };
+    content: string;
+    isAnswered:boolean;
+    isHighlighted: boolean;
+    likeCount: number;
+    likeID: string | undefined;
+}
+
+export function useRoom (roomID: string) {
+    const { user } = useAuth();
+    const [questions, setQuestions] = useState<QuestionType[]>([]);
+    const[title, setTitle] = useState('');
+
+    useEffect(() => {
+        const roomRef = database.ref(`rooms/${roomID}`);
+
+        roomRef.on('value', room => {
+            const databaseRoom = room.val();
+            const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+            const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+                return {
+                    id: key,
+                    content: value.content,
+                    author: value.author,
+                    isHighlighted: value.isHighlighted,
+                    isAnswered: value.isAnswered,
+                    likeCount: Object.values(value.likes ?? {}).length,
+                    likeID: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorID === user?.id)?.[0],
+                }
+            })
+            setTitle(databaseRoom.title);
+            setQuestions(parsedQuestions);
+        })
+
+        return () => {
+            roomRef.off('value');
+        }
+    }, [roomID, user?.id]);
+
+    return { questions, title }
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,6 +38,11 @@ export function Home() {
             return;
         }
 
+        if(roomRef.val().endedAt) {
+            alert('A sala foi fechada.');
+            return;
+        }
+
         history.push(`/rooms/${roomCode}`);
     }
 
